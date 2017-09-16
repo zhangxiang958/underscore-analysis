4efef4ac0e309f38f6f74791fd4590d6a176aa9d
zhangxiang <958033967@qq.com>
2017-09-16 14:55:13 +0800

    添加注释

--- underscore-1.8.3/underscore.js
+++ underscore-1.8.3/underscore.js
@@ -49,10 +49,11 @@
       nativeCreate = Object.create;
 
   // Naked function reference for surrogate-prototype-swapping.
-  // 
+  // 创建一个 "干净" 的构造函数用于实现优化后的组合继承.
   var Ctor = function(){};
 
   // Create a safe reference to the Underscore object for use below.
+  // 这里是创建了 underscore 对象.这个函数的目的其实就是为了创建 underscore 对象, 如果在 new 命令的时候有对象传进来, 那么就将这个对象传给新建对象的 _wrapped 属性存储起来.
   var _ = function(obj) {
     if (obj instanceof _) return obj;
     if (!(this instanceof _)) return new _(obj);
@@ -64,6 +65,13 @@
   // the browser, add `_` as a global object.
   // (`nodeType` is checked to ensure that `module`
   // and `exports` are not HTML elements.)
+  // 这段代码如何理解呢? 
+  // 这个是前端与 node.js 通用的模块封装模式, 
+  // 我们知道 exports 和 module.exports 是 node 模块化的显著标志, 
+  // 第一个先判断 exports 是否存在, 还有就是判断 exports 变量是否有 nodeType 属性, 
+  // 如果有, 说明 exports 变量是一个 html 元素, 所以不使用 node 方式模块加载, 
+  // 同理 module 变量也是这样.这里不再细讲 exports 和 module.exports 的区别, 
+  // 简单总结就是 exports 看作是 module.exports 的一个快照.
   if (typeof exports != 'undefined' && !exports.nodeType) {
     if (typeof module != 'undefined' && !module.nodeType && module.exports) {
       exports = module.exports = _;
@@ -74,11 +82,21 @@
   }
 
   // Current version.
+  // 当前版本为 1.8.3
   _.VERSION = '1.8.3';
 
   // Internal function that returns an efficient (for current engines) version
   // of the passed-in callback, to be repeatedly applied in other Underscore
   // functions.
+  // 一个内部动态实现绑定 this 的函数.
+  // 这个函数是什么意思？从整体上看并没有太大的意思，我们知道 this 的指向是动态的，
+  // 所以在实际开发中肯定免不了对函数的 this 值进行硬绑定的做法，但是 bind 函数会有兼容性问题， 
+  // 所以会倾向于使用 call 方法和 apply 方法，这两个原生函数在使用的时候区别就在于 call 后面是可以跟随 n 的参数，
+  // 而 apply 后面是跟随数组形式的参数的， 那为什么 underscore 源码需要将这两种方法区分呢？
+  // 可以看到 optimizeCb 函数会将传递参数少于或等于 4 个的采用 call 方法来绑定 this， 
+  // 而对于多于 4 个参数的方法则会采用 apply 方法来进行绑定，其实这是代码性能优化的手段，
+  // apply 方法在执行的时候其实是比 call 方法要慢得多， apply 方法在执行的时候需要对传进来的参数数组 进行深拷贝：apply 内部执行伪代码
+  // 请参考文章: https://github.com/zhangxiang958/underscore-sourcecodeAnalysis/issues/3
   var optimizeCb = function(func, context, argCount) {
     if (context === void 0) return func;
     switch (argCount) {
@@ -105,6 +123,7 @@
   // An internal function to generate callbacks that can be applied to each
   // element in a collection, returning the desired result — either `identity`,
   // an arbitrary callback, a property matcher, or a property accessor.
+  // cb 函数到底是做什么的? cb 函数是一个内部方法, 一下子可能看不出来它的功能, 但是我们可以借助 _.iteratee 函数来看, 
   var cb = function(value, context, argCount) {
     if (_.iteratee !== builtinIteratee) return _.iteratee(value, context);
     if (value == null) return _.identity;
@@ -116,14 +135,27 @@
   // External wrapper for our callback generator. Users may customize
   // `_.iteratee` if they want additional predicate/iteratee shorthand styles.
   // This abstraction hides the internal-only argCount argument.
+  // 根据文档 "_.iteratee 函数是用来生成可以应用到集合中每个元素的回调的, 返回想要的结果", 
+  // 而 _.iteratee 函数主要是用在了 _.each, _.map, _.find, _.filter 等等的这些需要回调的函数的.
   _.iteratee = builtinIteratee = function(value, context) {
     return cb(value, context, Infinity);
   };
 
   // Similar to ES6's rest param (http://ariya.ofilabs.com/2013/03/es6-and-rest-parameter.html)
   // This accumulates the arguments passed into an array, after a given index.
+  // 这个函数是用来模仿 ES6 语法中的拓展运算符的.
+  // items 代表的就是除了第一个定义的形参 array 之外, 剩余的其他参数都变成了 items 的一部分, 
+  // 而 items 会将剩余的所有参数集中起来, 放入一个数组里面, 所以 items 本身 就是一个数组, 
+  // 里面按顺序存放了除 array 之外的传进来的参数. 那么我们在没有 ES6 语法的情况下, 就需要使用 arguments 对象, 
+  // 将传进来的没有对应形参名的参数放入到一个数组里面, 所以我们当然需要知道函数本来已经定义了多少个已经命名了的形参的数量, 
+  // 假如原来函数已经定义了 2 个参数, 那么我们就从 arguments 的第三个参数也就是 arguments 转化后得到的数组的下标为 2 的元素开始放入到 rest 数组中. 
+  // 这也就是 startIndex 的意义. 然后通过一个闭包, 缓存起 startIndex 的值, 然后将模拟 rest 数组的生成:
+  // https://github.com/zhangxiang958/underscore-sourcecodeAnalysis/issues/5
+  // 为什么要使用这样一个 restArg 函数呢? 因为如果不使用这样的函数模拟 rest 运算符, 而是在每个函数调用 [].slice.call(arguments); 的话, 这样代码就耦合了
+  // 而且代码复用性不高.
   var restArgs = function(func, startIndex) {
     startIndex = startIndex == null ? func.length - 1 : +startIndex;
+    // 闭包缓存
     return function() {
       var length = Math.max(arguments.length - startIndex, 0),
           rest = Array(length),
@@ -131,6 +163,7 @@
       for (; index < length; index++) {
         rest[index] = arguments[index + startIndex];
       }
+      // 下面这里就是针对参数个数的多少进行 apply 和 call 的优化.
       switch (startIndex) {
         case 0: return func.call(this, rest);
         case 1: return func.call(this, arguments[0], rest);
@@ -146,6 +179,7 @@
   };
 
   // An internal function for creating a new object that inherits from another.
+  // 内部函数, 用于实现继承(组合继承)
   var baseCreate = function(prototype) {
     if (!_.isObject(prototype)) return {};
     if (nativeCreate) return nativeCreate(prototype);
@@ -155,12 +189,15 @@
     return result;
   };
 
+
+  // shallowProperty 函数是用来获取对象对应键名的值的, 
   var shallowProperty = function(key) {
     return function(obj) {
       return obj == null ? void 0 : obj[key];
     };
   };
-
+  // deepGet 函数则主要是在 property 或者 propertyOf 这两个根据键名获取键值的函数中, 
+  // 当输入的键名值为数组类型的时候, 采用遍历的方式 取值, 它是一个内部的工具函数. 
   var deepGet = function(obj, path) {
     var length = path.length;
     for (var i = 0; i < length; i++) {
@@ -174,8 +211,15 @@
   // should be iterated as an array or as an object.
   // Related: http://people.mozilla.org/~jorendorff/es6-draft.html#sec-tolength
   // Avoids a very nasty iOS 8 JIT bug on ARM-64. #2094
+  // 下面这里都是一些集合类函数的辅助函数.
+  // getLength 是一个获取对象 length 属性的工具方法, 而 MAX_ARRAY_INDEX 为 2 的 53 次方减 1, 
+  // 而这个是 javascript 中能表示的最大整数值.
   var MAX_ARRAY_INDEX = Math.pow(2, 53) - 1;
   var getLength = shallowProperty('length');
+  // isArrayLike 是为了判断一个对象是不是类数组对象
+  // 什么是类数组对象? 像我们常见的 arguments 对象, DOM 中的 NodeList 对象等等这些都是类数组对象, 
+  // 他们有 length 属性并且表示的是对象 拥有的元素的数量, 但是却没有数组的某些方法像 forEach, map 等等.
+  // 这也就是所谓的鸭子模型, 所以我们在使用 underscore 集合函数的时候要避免传入拥有 length 属性的对象, 否则会被当作数组使用.
   var isArrayLike = function(collection) {
     var length = getLength(collection);
     return typeof length == 'number' && length >= 0 && length <= MAX_ARRAY_INDEX;
@@ -187,6 +231,12 @@
   // The cornerstone, an `each` implementation, aka `forEach`.
   // Handles raw objects in addition to array-likes. Treats all
   // sparse array-likes as if they were dense.
+  // each 函数中 obj 就是你需要遍历的那个对象, 
+  // iteratee 是你定义的处理数据回调函数, context 是执行上下文, 回调函数的 this 值将会是 context. 
+  // 而对于第一句 iteratee = optimizeCb(iteratee, context);, 就是之前所说的视命名参数数量而使用 call 或 apply 动态生成回调的函数, 这里函数式编程的优势就体现了, 每个函数只负责单一的功能, 组合起来威力很大. 
+  // 然后对传进来的对象进行类型判断, 如果是类数组类型(有 length 属性, 并且是一个大于 1 小于 2 的 23 次方减 1 的数), 
+  // 那么就使用下标的形式也就是方括号的形式来遍历对象. 
+  // 如果不是, 那么就使用 _.keys 函数, 将对象的键名提取出来, 并返回键名数组, 然后根据键名数组中的键名, 逐个提取 obj 对象中的键值, 进行遍历. 
   _.each = _.forEach = function(obj, iteratee, context) {
     iteratee = optimizeCb(iteratee, context);
     var i, length;
@@ -195,6 +245,13 @@
         iteratee(obj[i], i, obj);
       }
     } else {
+      // 不知道大家在对象遍历的时候有没有疑问, 我在看的时候, 我觉得 _.keys 非常奇怪, 
+      // 为什么需要先提取键名数组, 使用 for...in 循环然后使用 hasOwnProperty 来判断对象是否拥有该键不可以吗? 
+      // 其实这里作者非常巧妙, 第一将获取键名的函数独立出去, 有利于其他函数的复用, 而且这样显得非常简洁.
+      // 第二 keys 数组如果能用原生的 Object.keys 当然最好, 但是会有兼容性问题, 所以需要使用 for...in 循环来兼容, 
+      // 所以不将 for...in 循环不写在 each 函数里面可以减低耦合度, 优化代码性能(for..in 不是必需执行的循环).
+      // 最后一点在于 IE9 的兼容问题, 在低于 IE9 的浏览器版本里面, 当对象中含有像 toString, valueOf, isPropertyOf, propertyIsEnumerable, hasOwnProperty, toLocaleString 这些属性名的键值对时, 
+      // 这些属性是无法使用 for...in 循环遍历出来的. 也就是 collectNonEnumProps 函数里面所做的内容.
       var keys = _.keys(obj);
       for (i = 0, length = keys.length; i < length; i++) {
         iteratee(obj[keys[i]], keys[i], obj);
@@ -204,6 +261,10 @@
   };
 
   // Return the results of applying the iteratee to each element.
+  // _.each 和 _.map 类似的, 只不过 _.map 会返回一个使用回调函数处理过的集合数据.这里有点奇怪, 
+  // 明明 _.each 处理逻辑与 _.map 处理逻辑类似的, 为什么 _.map 的代码写得这么简洁? 
+  // 这一点还不知道作者是出于什么考虑, 如果是我肯定是两个都是第二种写法. 还有一点疑问就是为什么 _.each 函数和 _.map 函数在生成回调的时候, 
+  // 一个采用 optimizeCb, 一个采用 cb 函数?
   _.map = _.collect = function(obj, iteratee, context) {
     iteratee = cb(iteratee, context);
     var keys = !isArrayLike(obj) && _.keys(obj),
@@ -217,6 +278,11 @@
   };
 
   // Create a reducing function iterating left or right.
+  // 如果是正常的 reduce 的话, 那么就是将输入的集合从左到右集合成一个数字, 
+  // 而对于 reduceRight 就是从右边开始, 从字面来看, 这两个 API 会有很大的相似点, 而在实际编写的时候也确实是这样的. 
+  // 作者先用一个 reducer 来构造 reduce 函数, 然后通过一个 flag 值来确定合并的方向, 也就是使用 dir, 
+  // 当 dir 为 1, 那么就是从左边开始, 当 dir 为 -1, 那么就是从右边开始. 
+  // 对于 createReduce 这个函数来说, 其实所做的时候就是通过闭包, 确定集合的方向, 然后还有调用 reducer 这个核心的方法. 
   var createReduce = function(dir) {
     // Wrap code that reassigns argument variables in a separate function than
     // the one that accesses `arguments.length` to avoid a perf hit. (#1991)
@@ -236,19 +302,26 @@
     };
 
     return function(obj, iteratee, memo, context) {
+      // 先用 initial 判断形参有多少个, 
       var initial = arguments.length >= 3;
+      // 然后进行 optimizeCb 回调函数包装优化. 回调是有 4 个参数的, 分别是 memo, value, index(key), list. memo 就是初始值, 
+      // list 里面的 item 值就会逐个相加到 memo 里面. 对于 reducer 这个函数:
       return reducer(obj, optimizeCb(iteratee, context, 4), memo, initial);
     };
   };
 
   // **Reduce** builds up a single result from a list of values, aka `inject`,
   // or `foldl`.
+  // 从左边开始将列表中的数集合为一个单一的数
   _.reduce = _.foldl = _.inject = createReduce(1);
 
   // The right-associative version of reduce, also known as `foldr`.
+  // 从右边开始将列表中的数集合为一个单一的数
   _.reduceRight = _.foldr = createReduce(-1);
 
   // Return the first value which passes a truth test. Aliased as `detect`.
+  // find 函数的作用在于说在列表中找到符合回调函数条件的元素, 一旦找到, 马上返回, 不会遍历整个列表. 
+  // 理解 find 先需要看 findIndex 和 findKey 这两个函数.
   _.find = _.detect = function(obj, predicate, context) {
     var keyFinder = isArrayLike(obj) ? _.findIndex : _.findKey;
     var key = keyFinder(obj, predicate, context);
@@ -257,6 +330,7 @@
 
   // Return all the elements that pass a truth test.
   // Aliased as `select`.
+  // _.filter 就是通过调用回调, 然后过滤出符合条件的元素, 然后加入到 result 数组中, 然后最终得到这个数组. 
   _.filter = _.select = function(obj, predicate, context) {
     var results = [];
     predicate = cb(predicate, context);
@@ -267,12 +341,15 @@
   };
 
   // Return all the elements for which a truth test fails.
+  // 所谓 _.reject 就是 filter 的反函数, 它是过滤出不符合条件的元素, 然后将不符合 条件的元素加入到新数组中, 最终得到这个数组.
   _.reject = function(obj, predicate, context) {
     return _.filter(obj, _.negate(cb(predicate)), context);
   };
 
   // Determine whether all of the elements match a truth test.
   // Aliased as `all`.
+  // every 函数就是为了让集合中每个 item 都能使用回调来执行, 然后如果回调都是返回 true, 则最后返回 true, 
+  // 一旦有一个 item 执行回调之后返回 false, 那么 every 返回的也是 false. 也就是验证每个 item 是否都符合规则.
   _.every = _.all = function(obj, predicate, context) {
     predicate = cb(predicate, context);
     var keys = !isArrayLike(obj) && _.keys(obj),
@@ -286,6 +363,7 @@
 
   // Determine if at least one element in the object matches a truth test.
   // Aliased as `any`.
+  // 而对于 some 就是监测集合中是否存在符合规则的, 如果一旦找到一个, 那么就结束循环然后返回 true, 否则就返回 false.
   _.some = _.any = function(obj, predicate, context) {
     predicate = cb(predicate, context);
     var keys = !isArrayLike(obj) && _.keys(obj),
